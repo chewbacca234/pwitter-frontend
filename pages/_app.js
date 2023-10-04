c98ccf19d99285462af341cb4c90ce66ff102390
chewbacca234 <carine.dupuis.es@gmail.com>
2023-10-04 19:08:16 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,34 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import { users } from '../reducers/users'
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const reducers = combineReducers({ users });
+const persistConfig = { key: 'applicationName', storage };
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});
+
+const persistor = persistStore(store);
 
 function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Next.js App</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Head>
+          <title>Pwitter</title>
+        </Head>
+        <Component {...pageProps} />
+      </PersistGate>
+
+    </Provider>
   );
 }
 
